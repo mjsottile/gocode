915c630b8403a0c52ebebbad3c0e37f3b2a5ef9f
Matthew Sottile <mjsottile@me.com>
2011-09-08 00:30:43 -0700

    beginning

---
+++ sexpr/sexpr.go
@@ -0,0 +1,14 @@
+//
+// s-expression parser, based on Rob Pike's 2011 lexical scanning in go
+// talk.
+//
+// matt@galois.com // sept. 2011
+//
+package main
+
+import "fmt"
+
+// main will be for testing for now
+func main() {
+  fmt.Println("hi")
+}
